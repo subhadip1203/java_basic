becb670c8de13347ead6f40c25a0770f29c84611
subhadip1203 <subhadip1203@gmail.com>
2022-05-14 16:11:19 +0530

    Spring core basic 1

--- basic/spring/spring_part_1/src/main/java/com/subhadip/spring_part_1/App.java
+++ basic/spring/spring_part_1/src/main/java/com/subhadip/spring_part_1/App.java
@@ -1,13 +1,24 @@
 package com.subhadip.spring_part_1;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+    	ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+    	try {
+    		Student student1 = (Student) context.getBean("student1");
+    		System.out.println(student1);
+    	} finally {
+    		((ClassPathXmlApplicationContext) context).close();
+    	}
+    	
+    	
+    
     }
 }
