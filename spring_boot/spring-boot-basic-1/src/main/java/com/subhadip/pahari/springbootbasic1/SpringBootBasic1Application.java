4c8c111dbe2dee505bd57337445fc0b271169a82
subhadip1203 <subhadip1203@gmail.com>
2022-06-22 21:19:18 +0530

    spring boot @ComponentScan

--- spring_boot/spring-boot-basic-1/src/main/java/com/subhadip/pahari/springbootbasic1/SpringBootBasic1Application.java
+++ spring_boot/spring-boot-basic-1/src/main/java/com/subhadip/pahari/springbootbasic1/SpringBootBasic1Application.java
@@ -2,12 +2,16 @@ package com.subhadip.pahari.springbootbasic1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootBasic1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootBasic1Application.class, args);
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringBootBasic1Application.class, args);
+
+		SpringComponent component = applicationContext.getBean(SpringComponent.class);
+		System.out.println(component.getMessage());
 		System.out.println("server running ...");
 	}
 
---
+++ spring_boot/spring-boot-basic-1/src/main/java/com/subhadip/pahari/springbootbasic1/SpringComponent.java
@@ -0,0 +1,10 @@
+package com.subhadip.pahari.springbootbasic1;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringComponent {
+  public String getMessage() {
+    return "Hello World";
+  }
+}
