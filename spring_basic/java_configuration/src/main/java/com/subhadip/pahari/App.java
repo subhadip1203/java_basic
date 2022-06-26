ebfbc23697098ac46a189bede020e5fc0f051734
subhadip1203 <subhadip1203@gmail.com>
2022-06-26 23:33:27 +0530

    java spring @Config type

---
+++ spring_basic/java_configuration/src/main/java/com/subhadip/pahari/App.java
@@ -0,0 +1,13 @@
+package com.subhadip.pahari;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
